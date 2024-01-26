5d6e017831ac9ebc10e3f7607df170ceebf58374
Colin-Chen630 <3229291803@qq.com>
2024-01-25 19:28:25 -0600

    upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# GANimate
+CycleGAN implementation which allows image-to-image translation between human faces and cartoon faces.
+
+# Dependencies
+* Python Version 3.8.5+
+* PyTorch Version 1.8.0
+
+# Instructions for Training
+* If you have not yet downloaded the data, do so by uncommenting the "Download data" portion in train.py.
+* Run train.py
+
+**TODO :** add file for testing pre-existing weights.
+
---
+++ dataset.py
@@ -0,0 +1,63 @@
+import numpy as np
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+
+class ImageData(Dataset):
+    def __init__(self, dirs=["D:\desktop\ganimate-main\dataA", "D:\desktop\ganimate-main\BitmojiDataset\images"], split="train", transform=None):
+        self.transform = transform
+        self.files_A = []
+        self.files_B = []
+        
+        if split == "train":
+          for filename in os.listdir(dirs[0]):
+              base, ext = os.path.splitext(filename)
+              try:
+                  filenum = int(base)
+              except ValueError:
+                  continue
+              
+              if 0 <= filenum < 1899:
+                  self.files_A.append(dirs[0] + '/' + filename)
+          for filename in os.listdir(dirs[1]):
+            base, ext = os.path.splitext(filename)
+            try:
+                filenum = int(base)
+            except ValueError:
+                continue
+            
+            if 0 <= filenum < 1899:
+                self.files_B.append(dirs[1] + '/' + filename)
+                # end if
+            # end for files in root_dir
+        else:
+          for filename in os.listdir(dirs[0]):
+              base, ext = os.path.splitext(filename)
+              try:
+                  filenum = int(base)
+              except ValueError:
+                  continue
+              
+              if 1899 <= filenum < 1999:
+                  self.files_A.append(dirs[0] + '/' + filename)
+          for filename in os.listdir(dirs[1]):
+            base, ext = os.path.splitext(filename)
+            try:
+                filenum = int(base)
+            except ValueError:
+                continue
+            
+            if 1899 <= filenum < 1999:
+                self.files_B.append(dirs[1] + '/' + filename)
+                # end if
+            # end for files in root_dir
+
+    # len overload, returns the number of files in the loader
+    def __getitem__(self, index):
+        item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+        item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]))
+
+        return {"A": item_A, "B": item_B}
+
+    def __len__(self):
+        return max(len(self.files_A), len(self.files_B))
